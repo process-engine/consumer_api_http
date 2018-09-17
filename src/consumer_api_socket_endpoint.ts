ef6066ad7c79bb50a616866fd6d4d86836f11506
5ebastianMeier <sebastian.meier@5minds.de>
2018-09-17 15:31:37 +0200

    fix typo

--- src/consumer_api_socket_endpoint.ts
+++ src/consumer_api_socket_endpoint.ts
@@ -33,7 +33,7 @@ export class ConsumerApiSocketEndpoint extends BaseSocketEndpoint {
   public initializeEndpoint(socketIo: SocketIO.Namespace): void {
 
     socketIo.on('connect', (socket: SocketIO.Socket) => {
-      const identity: string = socket.handshake.headers['Authorization'];
+      const identity: string = socket.handshake.headers['authorization'];
 
       const connection: IConnection = {
         identity,
