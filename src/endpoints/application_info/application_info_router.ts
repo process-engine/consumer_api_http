78c145b3bf793a7ee8cb70489997676ca2168015
ElRaptorus <christian.werner@5minds.de>
2020-02-26 13:11:36 +0100

    :bug: Add workaround for middleware registry bug

--- src/endpoints/application_info/application_info_router.ts
+++ src/endpoints/application_info/application_info_router.ts
@@ -1,5 +1,7 @@
 import {wrap} from 'async-middleware';
+import {NextFunction, Request, Response} from 'express';
 
+import {BaseError} from '@essential-projects/errors_ts';
 import {BaseRouter} from '@essential-projects/http_node';
 
 import {restSettings} from '@process-engine/consumer_api_contracts';
@@ -26,7 +28,20 @@ export class ApplicationInfoRouter extends BaseRouter {
   private registerRoutes(): void {
     const controller = this.applicationInfoController;
 
-    this.router.get(restSettings.paths.getApplicationInfo, wrap(controller.getApplicationInfo.bind(controller)));
+    this.router.get(restSettings.paths.getApplicationInfo, errorHandler, wrap(controller.getApplicationInfo.bind(controller)));
   }
 
+}
+
+// TODO:
+// There is a bug with either the HTTP extension, express, or the BaseRouter, which causes the "resolveIdentity" middleware to be applied to
+// ALL of the consumer api routers, even if they do not make explicit use of that middleware.
+// Since this route is designed to be used without the need of any identity, we need to intercept the 401 errors here
+// that the middleware throws, if no auth token is provided.
+function errorHandler(error: BaseError, request: Request, response: Response, next: NextFunction): void {
+  if (error.code === 401) {
+    next();
+  } else {
+    next(error);
+  }
 }
